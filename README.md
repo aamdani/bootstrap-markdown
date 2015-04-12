5e36284d8dddbb2963801e1d58a62e75724af7fe
Ahad L. Amdani <ahad.amdani@gmail.com>
2015-04-12 18:55:15 -0500

    Minimum package content for bootstrap-markdown.

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple wrapper package in Meteor for bootstrap-markdown with the official twbs:bootstrap dependency and appropriate markdown-to and marked dependencies.
---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'brwn:bootstrap-markdown',
+  version: '1.0.0',
+  // Brief, one-line summary of the package.
+  summary: 'bootstrap-markdown wrapper using the official bootstrap package dependency and appropriate markdown-to and marked dependencies.',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/aamdani/bootstrap-markdown',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.on_use(function (api) {
+    api.versionsFrom('METEOR@1.1');
+    // dependecies required by package
+    api.use('twbs:bootstrap@3.3.4','client')
+    // adding the required file for package
+    api.add_files('lib/bootstrap-markdown.js', 'client');
+    api.add_files('lib/bootstrap-markdown.min.css', 'client');
+});
