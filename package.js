9c5a6207007931fee918c52291019467ed23c253
Ahad L. Amdani <ahad.amdani@gmail.com>
2015-04-12 19:34:58 -0500

    Updated CSS and published to atmosphere.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'brwn:bootstrap-markdown',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'bootstrap-markdown wrapper using the official bootstrap package dependency.',
   // URL to the Git repository containing the source code for this package.
@@ -13,7 +13,9 @@ Package.describe({
 Package.on_use(function (api) {
     api.versionsFrom('METEOR@1.1');
     // dependecies required by package
-    api.use('twbs:bootstrap@3.3.4','client')
+    api.use('twbs:bootstrap@3.3.4','client');
+    api.use('brwn:marked@1.0.0', 'client');
+    api.use('brwn:to-markdown@1.0.0', 'client');
     // adding the required file for package
     api.add_files('lib/bootstrap-markdown.js', 'client');
     api.add_files('lib/bootstrap-markdown.min.css', 'client');
